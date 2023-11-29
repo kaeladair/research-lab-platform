537103e27f28a33f06372815a28072fb7298906c
Kael Adair <kja75810@gmail.com>
2023-11-29 12:39:13 -0800

    Add popup for event creation

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,24 +3,23 @@ import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { v4 as uuid } from 'uuid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function MyCalendar() {
   const [events, setEvents] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [eventName, setEventName] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
 
   const handleSelect = (info) => {
-    const { start, end } = info;
-    const eventNamePrompt = prompt("Enter event name");
-    if (eventNamePrompt) {
-      setEvents([
-        ...events,
-        {
-          start,
-          end,
-          title: eventNamePrompt,
-          id: uuid(),
-        },
-      ]);
-    }
+    setSelectedDate(info);
+    setOpen(true);
   };
 
   return (
@@ -37,6 +36,41 @@ function MyCalendar() {
         plugins={[timeGridPlugin, interactionPlugin]}
         initialView="timeGridWeek"
       />
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Create New Event</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Please enter the event name.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Event Name"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setEventName(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={() => {
+            if (eventName) {
+              setEvents([
+                ...events,
+                {
+                  title: eventName,
+                  start: selectedDate.startStr,
+                  end: selectedDate.endStr,
+                  allDay: selectedDate.allDay,
+                },
+              ]);
+            }
+            setOpen(false);
+          }}>Create</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
